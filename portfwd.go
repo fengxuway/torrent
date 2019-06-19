540f433d0b8e9ff0750563c7a503b353373e6605
冯旭 <fengxu6@staff.weibo.com>
2019-06-19 15:22:08 +0800

    delete log for portfwd and storage

--- portfwd.go
+++ portfwd.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"time"
 
-	#flog "github.com/anacrolix/log"
+	//flog "github.com/anacrolix/log"
 	"github.com/elgatito/upnp"
 )
 
@@ -28,7 +28,7 @@ func (cl *Client) forwardPort() {
 	cl.unlock()
 	ds := upnp.Discover(0, 2*time.Second)
 	cl.lock()
-	#flog.Default.Handle(flog.Fmsg("discovered %d upnp devices", len(ds)))
+	//flog.Default.Handle(flog.Fmsg("discovered %d upnp devices", len(ds)))
 	port := cl.incomingPeerPort()
 	cl.unlock()
 	for _, d := range ds {
